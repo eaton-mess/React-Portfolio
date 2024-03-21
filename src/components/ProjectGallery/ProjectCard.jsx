de108199c2b30dd79db83e070463ab08fe51bbda
eatontechnology <eaton.technology@gmail.com>
2024-03-21 13:01:37 +0000

    made project images fit

--- src/components/ProjectGallery/ProjectCard.jsx
+++ src/components/ProjectGallery/ProjectCard.jsx
@@ -8,7 +8,7 @@ function ProjectCard({ project }) {
                 <Card.Title>{project.title}</Card.Title>
                 <Card.Text>{project.description}</Card.Text>
                 <div>
-                    <Card.Img variant="top" src={project.image} />
+                    <Card.Img variant="top" src={project.image} style={{ maxWidth: "100%" }} />
                 </div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
                     <Button variant="success" href={project.deployed} style={{ marginRight: "15px" }}>Deployed</Button>
