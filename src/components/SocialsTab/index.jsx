2f3dd7e0b74c122c04999dfed5a661a015647b88
eatontechnology <eaton.technology@gmail.com>
2024-03-23 22:01:51 +0000

    amended socials to remove cv

--- src/components/SocialsTab/index.jsx
+++ src/components/SocialsTab/index.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
-import { faFolder } from '@fortawesome/free-solid-svg-icons';
 
 const SocialsTab = () => {
     return (
-        <Nav className="ml-auto" style={{ position: 'fixed', top: '0', right: '20px', maxWidth: '16em', background: 'black', zIndex: '9999' }}>
+        <Nav className="ml-auto" style={{ position: 'fixed', top: '0', right: '5.5em', maxWidth: '16em', background: 'black', zIndex: '9999' }}>
             <Nav.Link href="https://github.com/eaton-mess/" target="_blank">
                 <FontAwesomeIcon icon={faGithubSquare}
                     style={{
@@ -28,14 +27,6 @@ const SocialsTab = () => {
                     style={{
                         color: 'white',
                         backgroundColor: 'transparent'
-                    }}
-                    size="2x" />
-            </Nav.Link>
-            <Nav.Link href="https://drive.google.com/file/d/1aGbPapXGVhgwpIJC7SizT-Fp-moFJAxe/view?usp=drive_link" target="_blank">
-                <FontAwesomeIcon icon={faFolder}
-                    style={{
-                        color: 'white',
-                        backgroundColor: 'transparent',
                     }}
                     size="2x" />
             </Nav.Link>
